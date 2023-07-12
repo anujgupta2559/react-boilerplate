bf586f96b4d7cf0bdea5adf4d97193ece1f96ae1
Anuj Gupta <anujgupta@cedcommerce.com>
2023-07-12 07:25:57 +0000

    test

--- index1-new.js
+++ index1-new.js
@@ -4,7 +4,5 @@ import { AmplifyClient } from "@aws-sdk/client-amplify";
 const client = new AmplifyClient({
     region : "us-east-2",
     credentials: {
-        accessKeyId : "AKIAF6BAFJKR45SAWSZ5",
-        // secretAccessKey: "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"
-    }
+        accessKeyId : "AKIAF6BAFJKR45SAWSZ5",    }
 })
