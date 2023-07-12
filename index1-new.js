0dbecadc17ce099e65e93e9829d4fc3da9d28946
Anuj Gupta <anujgupta@cedcommerce.com>
2023-07-12 07:25:37 +0000

    test

--- index1-new.js
+++ index1-new.js
@@ -5,6 +5,6 @@ const client = new AmplifyClient({
     region : "us-east-2",
     credentials: {
         accessKeyId : "AKIAF6BAFJKR45SAWSZ5",
-        secretAccessKey: "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"
+        // secretAccessKey: "qECyRVB49aSbr0aI7pkLgv5bxM08JZRMZeK9SBjp"
     }
 })
