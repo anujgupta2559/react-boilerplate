347af86dd649628af6699d441cf5e35bde6a1f7c
Anuj Gupta <anujgupta@cedcommerce.com>
2023-07-28 12:08:11 +0000

    added GTM

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function pushToLayer(e){
+    const dataLayer = window.dataLayer
+    dataLayer.push({
+      e
+    })
+  }
   return (
-    <div className="App">
+
+<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button className='GASample-button' id="GA-7568" href="https://google.com" onClick={(e)=>pushToLayer(e)}>Click me</button>
         <a
           className="App-link"
           href="https://reactjs.org"
