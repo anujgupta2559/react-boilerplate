9c35c79c4738f38640154c89a7ff94cb3274eaa3
Anuj Gupta <anujgupta@cedcommerce.com>
2023-07-31 06:42:58 +0000

    added datalayer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import TagManager from 'react-gtm-module';
+import logo from "./logo.svg";
+import "./App.css";
+import TagManager from "react-gtm-module";
 
 function App() {
-
   const config = {
-    gtmId: "GTM-MHXXQ3CC"
-  }
+    gtmId: "GTM-MHXXQ3CC",
+  };
 
-  TagManager.initialize(config)
-  
+  TagManager.initialize(config);
+
+  const pushDataLayer = () => {
+    // console.log("Hey ...")
+    window.dataLayer.push({ event: "customEvent", data: { name: "test", region: "India" } });
+  };
+
   return (
-
-<div className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <button className='GASample-button' id="GA-7568" href="https://google.com">Click me</button>
+        <button
+          className="GASample-button"
+          id="GA-7568"
+          onClick={() => pushDataLayer()}
+          href="https://google.com"
+        >
+          Click me
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
